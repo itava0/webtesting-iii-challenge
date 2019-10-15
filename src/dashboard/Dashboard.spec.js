091595f4350780b94cbd50ee144e90ebc2a80cae
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-15 11:50:49 -0400

    Created test for the dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+
+test("Dashboard renders to screen", () => {
+  render(<Dashboard />);
+});
+
+test("Gate should default to unlocked and open position", () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText(/open/i));
+  expect(getByText(/unlocked/i));
+});
+
+test("Show the controls and display", () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
